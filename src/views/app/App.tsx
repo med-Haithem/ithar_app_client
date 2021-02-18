9cf5aa9b55687e4d656a5dae890218c90fdc735b
mohamed <benhassen@brainstack.net>
2021-02-18 18:09:37 +0100

    added routes configuration

--- src/views/app/App.tsx
+++ src/views/app/App.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import "./App.less";
+import logo from "./logo.svg";
 
-type Props = {};
-
-export const App = (props: Props) => {
-  return <div></div>;
-};
+export const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.tsx</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
+  </div>
+);
--- src/views/index.tsx
+++ src/views/index.tsx
@@ -1,1 +1,2 @@
 export const mock = "";
+export * from "./app";
